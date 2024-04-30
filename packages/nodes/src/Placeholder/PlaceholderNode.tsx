25677ec9f09c29bdb304e6841861cae74c2b4429
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-04-30 15:35:22 +0200

    fix: release new version

--- packages/nodes/src/Placeholder/PlaceholderNode.tsx
+++ packages/nodes/src/Placeholder/PlaceholderNode.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { GridNodeLayoutSelector } from "../Containers/GridNode/GridNodeLayoutSelector";
 import { Placeholder } from "./Placeholder";
 
+
 type PlaceholderNodeProps = {
     content: string;
     children?: React.ReactNode
